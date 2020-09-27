6c1a726cdc0356a033c63b9c65a82a7ed51d1e0d
Alexey Kochubey <alexkochubey@icloud.com>
2020-09-28 01:04:39 +0400

    Task 007 is complete

---
+++ src/main/java/com/epam/university/java/core/task007/Task007Impl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task007;
+
+import java.util.*;
+
+
+public class Task007Impl implements Task007 {
+    @Override
+    public Collection<Integer> multiplyPolynomial(Collection<Integer> first, Collection<Integer> second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < first.size() + second.size() - 1; i++) {
+            result.add(i, 0);
+        }
+
+        for (int i = 0; i < first.size(); i++) {
+            for (int j = 0; j < second.size(); j++) {
+                int product = result.get(i + j)
+                        + ((List<Integer>) first).get(i) * ((List<Integer>) second).get(j);
+                result.remove(i + j);
+                result.add(i + j, product);
+            }
+
+        }
+        return result;
+    }
+}
