e549c57df2fac51c863c0296eda658a68f5a6e7f
Alexey Kochubey <alexkochubey@icloud.com>
2020-10-26 12:13:15 +0400

    Task 036 is resolved

--- src/main/java/com/epam/university/java/core/task036/IntegratorImpl.java
+++ src/main/java/com/epam/university/java/core/task036/IntegratorImpl.java
@@ -1,2 +1,28 @@
-package com.epam.university.java.core.task036;public class IntegratorImpl {
+package com.epam.university.java.core.task036;
+
+import java.util.function.Function;
+
+public class IntegratorImpl implements Integrator {
+    public static final double INCREMENT = 1E-4;
+
+    @Override
+    public double integrate(double left, double right, Function<Double, Double> function) {
+        double area = 0;
+        double modifier = 1;
+        if (left > right) {
+            double tempA = left;
+            left = right;
+            right = tempA;
+            modifier = -1;
+        }
+        for (double i = left + INCREMENT; i < right; i += INCREMENT) {
+            double dFromA = i - left;
+            area += (INCREMENT / 2)
+                    *
+                    (function.apply(left + dFromA)
+                            +
+                            function.apply(left + dFromA - INCREMENT));
+        }
+        return (Math.round(area * 10000.0) / 10000.0) * modifier;
+    }
 }
--- src/main/java/com/epam/university/java/core/task036/Task036Impl.java
+++ src/main/java/com/epam/university/java/core/task036/Task036Impl.java
@@ -1,2 +1,13 @@
-package com.epam.university.java.core.task036;public class Task036Impl {
+package com.epam.university.java.core.task036;
+
+import java.util.function.Function;
+
+public class Task036Impl implements Task036 {
+    @Override
+    public double integrate(Function<Double, Double> function,
+                            Integrator integrator,
+                            double limitLeft, double limitRight) {
+        IntegratorImpl integr = new IntegratorImpl();
+        return integr.integrate(limitLeft, limitRight, function);
+    }
 }
