7185495a724dedbf2d4154f14ba57f8add44fd07
Alexey Kochubey <alexkochubey@icloud.com>
2020-10-26 11:58:20 +0400

    Task 015 is resolved

--- src/main/java/com/epam/university/java/core/task015/PointImpl.java
+++ src/main/java/com/epam/university/java/core/task015/PointImpl.java
@@ -1,2 +1,56 @@
-package com.epam.university.java.core.task015;public class PointImpl {
+package com.epam.university.java.core.task015;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class PointImpl implements Point {
+
+    private double x;
+    private double y;
+
+    public PointImpl() {
+    }
+
+    public PointImpl(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PointImpl point = (PointImpl) o;
+        return Double.compare(point.x, x) == 0
+                && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
