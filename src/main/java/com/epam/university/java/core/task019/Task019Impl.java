5ca3bd8ea2954186213d61739985212fd6477417
Alexey Kochubey <alexkochubey@icloud.com>
2020-10-26 12:12:40 +0400

    Task 019 is resolved

--- src/main/java/com/epam/university/java/core/task019/Task019Impl.java
+++ src/main/java/com/epam/university/java/core/task019/Task019Impl.java
@@ -1,2 +1,19 @@
-package com.epam.university.java.core.task019;public class Task019Impl {
+package com.epam.university.java.core.task019;
+
+public class Task019Impl implements Task019 {
+    @Override
+    public void invokeAction(Robot robot, RobotCommand command) {
+        robot.invokeAction(command);
+    }
+
+    @Override
+    public boolean isOnStartPosition(Robot robot) {
+        if (robot == null) {
+            throw new IllegalArgumentException();
+        }
+        if (robot.getPosition().getX() == 0 && robot.getPosition().getY() == 0) {
+            return true;
+        }
+        return false;
+    }
 }
