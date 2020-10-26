c0f78a86aa8edf8875b896d21da93bdee4e148ca
Alexey Kochubey <alexkochubey@icloud.com>
2020-10-26 12:03:40 +0400

    Tasks 051, 052 & 050 are resolved

--- src/main/java/com/epam/university/java/core/task051/Task051Impl.java
+++ src/main/java/com/epam/university/java/core/task051/Task051Impl.java
@@ -1,2 +1,24 @@
-package com.epam.university.java.core.task051;public class Task051Impl {
+package com.epam.university.java.core.task051;
+
+import java.util.Arrays;
+
+public class Task051Impl implements Task051 {
+
+    @Override
+    public String replace(String source) {
+        if (source == null || source.trim().equals("")
+                || source.equals("the") || source.contains("THE")) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] sourceArray = source.split(" ");
+        for (int i = 0; i < sourceArray.length; i++) {
+            if (sourceArray[i].equals("the")) {
+                sourceArray [i] = String.valueOf(sourceArray[i + 1].charAt(0))
+                        .matches("[aeyoui]") ? "an" : "a";
+            }
+        }
+
+        return String.join(" ", sourceArray);
+    }
 }
--- src/main/java/com/epam/university/java/core/task052/Task052Impl.java
+++ src/main/java/com/epam/university/java/core/task052/Task052Impl.java
@@ -1,2 +1,43 @@
-package com.epam.university.java.core.task052;public class Task052Impl {
+package com.epam.university.java.core.task052;
+
+public class Task052Impl implements Task052 {
+
+    @Override
+    public boolean validateCard(long number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        String numberString = String.valueOf(number);
+        String checkDigit = String.valueOf(numberString.charAt(numberString.length() - 1));
+
+        return luhnTest(numberString.substring(0, numberString.length() - 1)).equals(checkDigit);
+    }
+
+    private String luhnTest(final String value) {
+        int sum = 0;
+        int nDigits = value.length();
+        int parity = nDigits % 2;
+        for (int i = nDigits; i > 0; i--) {
+            int digit = Character.getNumericValue(value.charAt(i - 1));
+            if (i % 2 == parity) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+                sum += digit;
+            }
+        }
+        for (int i = nDigits; i > 0; i--) {
+            int digit = Character.getNumericValue(value.charAt(i - 1));
+            if (i % 2 != parity) {
+                sum += digit;
+            }
+        }
+        if ((((sum % 10) - 10) * -1) == 10) {
+            return ("0");
+        } else {
+            return ("" + (((sum % 10) - 10) * -1));
+        }
+    }
 }
