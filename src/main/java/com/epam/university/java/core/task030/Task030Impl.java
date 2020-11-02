c9608f00f0d69a1c4901b12aba9bbe95662c8033
Alexey Kochubey <alexkochubey@icloud.com>
2020-11-03 00:28:48 +0400

    Task 030 is complete

--- src/main/java/com/epam/university/java/core/task030/Task030Impl.java
+++ src/main/java/com/epam/university/java/core/task030/Task030Impl.java
@@ -1,38 +1,48 @@
 package com.epam.university.java.core.task030;
 
 import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Task030Impl implements Task030 {
     @Override
     public int daysBetweenDates(LocalDate dateStart, LocalDate dateEnd) {
-        return 0;
+        if (dateStart == null || dateEnd == null) {
+            throw new IllegalArgumentException();
+        }
+        return (int) DAYS.between(dateStart, dateEnd);
     }
 
     @Override
     public LocalDate adjustDays(LocalDate dateStart, int daysToAdd) {
-        return null;
+        return dateStart.plusDays(daysToAdd);
     }
 
     @Override
     public long distanceBetween(Instant instantStart, Instant instantEnd) {
-        return 0;
+        Duration between = Duration.between(instantStart, instantEnd);
+        return between.getSeconds();
     }
 
     @Override
     public DayOfWeek getDayOfWeek(LocalDate localDate) {
-        return null;
+        return localDate.getDayOfWeek();
     }
 
     @Override
     public LocalDate getNextWeekend(LocalDate localDate) {
-        return null;
+        return localDate.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
     }
 
     @Override
     public LocalTime getLocalTime(String timeString) {
-        return null;
+
+        return LocalTime.parse(timeString, DateTimeFormatter.ofPattern("hh:mma"));
     }
 }
